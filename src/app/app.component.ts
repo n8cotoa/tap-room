2bf40a71268a8d8efa39ac8ab4ae58be14a71ed9
Nate Cottle and Reese Glasscock <nate@>
2018-08-15 11:10:13 -0700

    Add table of kegs with info, edit functionality, add new keg functionality, start sale functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from '../models/keg.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'I\'d Tap That';
+
+  kegs: Keg[] = [
+    new Keg('Kegger', 'Keg & Friends', 3.50, 4.5),
+    new Keg('Hardest Cider', 'Keg Land', 4.50, 7.5),
+    new Keg('Reds Pale Ale', 'Are You Keggers?', 5.50, 8.5)
+  ];
+
+  selectedKeg = null;
+
+  newKeg = null;
+
+  sale = null;
+
+  editKeg(keg: Keg) {
+    if (this.selectedKeg === keg) {
+      this.selectedKeg = null;
+    } else {
+    this.selectedKeg = keg;
+    }
+  }
+
+  endEdit() {
+    this.selectedKeg = null;
+  }
+
+  addKeg() {
+    this.newKeg = true;
+  }
+
+  addNewKeg(name: string, brand: string, price: number, alcContent:number){
+    if (name === undefined || brand === undefined || price === undefined || alcContent === undefined) {
+      this.newKeg = null;
+    } else {
+      let keg = new Keg(name, brand, price, alcContent);
+      this.kegs.push(keg);
+      this.newKeg = null;
+      console.log(this.kegs)
+    }
+  }
+
+  startSale(keg: Keg) {
+    if (this.sale === keg) {
+      this.sale = null;
+    } else {
+    this.sale = keg;
+    }
+  }
+
 }
---
+++ src/models/keg.model.ts
@@ -0,0 +1,12 @@
+export class Keg {
+
+  public pintsLeft: number = 124;
+  constructor(
+    public name: string,
+    public brand: string,
+    public price: number,
+    public alcContent: number
+  ) {}
+
+
+}
